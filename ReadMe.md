e205a6e7ee66ab4c0046d53969e4521f2029a8e8
jahson_k <infojahson@gmail.com>
2023-03-28 09:14:23 +0300

    Done cleaning

--- ReadMe.md
+++ ReadMe.md
@@ -22,7 +22,7 @@ The data is provided in csv format which can easily be opened in excel or google
   is duplicated and therefore using SQL Common Table Expressions(CTE) i removed the duplicate row by executing the following query. Having over 70 columns in the dataset, I used just a few columns i.e ID, LongName, Name, Nationality, Positions, DuplicateCount to identify the duplicates. In this case i made the assumption that there are no two players with the same name, coming from the same country and playing the same position. 
   The query in the CTE returns a count of 1 for non repeating rows and a value greater than one for repeating rows. 
 
-```
+```sql
   WITH CTE (ID, LongName, Name, Nationality, Positions, DuplicateCount)
 AS (SELECT
   ID,
@@ -40,7 +40,7 @@ WHERE DuplicateCount > 1
 
 You can check for duplicates after executing the above query with the following query.
 
-```
+```sql
 SELECT COUNT(ID), COUNT(DISTINCT ID) from FifaData
 ```
 The query should result should be equal for both *COUNT(ID)* and *COUNT(DISTINCT ID)* otherwise duplicates still exists.
@@ -50,7 +50,7 @@ The query should result should be equal for both *COUNT(ID)* and *COUNT(DISTINCT
 After removing duplicates, its time to check the data in each column and then identity the columns that need cleaning. The first column was the height column whose data is in foot and inches. In order to perform aggregations on this column or any mathematical calculations then this column has to be transformed into a decimal or integer data type. This means converting the foot and inches to either centimeters or meters. In this case is used the query below to convert the height into Centimeters.
 Notice that a series of SQL functions are used to achieve the goal. These include **REPLACE** that is used to replace values in a string with another value, **LEFT** which is a function that returns all characters from the left of the string to a given position and the **CAST** which converts the result into INT data type before perfoming the conversion. 
 
-```
+```sql
 UPDATE FifaData
 SET Height = (CAST(LEFT(Height, 1) AS int) * 30.48) + (CAST(REPLACE(REPLACE(Height, RIGHT(Height, 1), '')
 , LEFT(Height, 1) + '''', '') AS int) * 2.54)
@@ -61,7 +61,7 @@ SET Height = (CAST(LEFT(Height, 1) AS int) * 30.48) + (CAST(REPLACE(REPLACE(Heig
 
  The next column to clean is the Weight of the players. Looking carefully at the data the weight is in lbs with a trailing 'lbs' at the end of each value. By replacing the 'lbs' text on the values the weight column is cleaned. You may however opt to convert the pounds into kgs by multiplying the result by 0.454
 
- ```
+ ```sql
  UPDATE FifaData
 SET Weight = REPLACE(Weight, 'lbs', '')
 
@@ -71,7 +71,7 @@ SET Weight = REPLACE(Weight, 'lbs', '')
  ### Wage, Value and Release Clause
 The wage,value and release clause data is all in a similar format and the same criteria can be applied while cleaning the data. To begin with, we do away with the Pound symbol at the start of each values, then check for the leters **M** and **K** and remove them as well using the **REPLACE** function. Now, we are left with a decimal value that we then multiply with either 1000 or 1000000 for **K** and **M** values respectively. In the event that these columns are missing the two letters, then we just keep the original value 
 
-```
+```sql
 
 
 UPDATE FifaData
@@ -110,7 +110,7 @@ END)
 
  **PATINDEX** function in SQL returns the position of a pattern or value in a string. If the value or pattern is not found then it returns 0. In this case we are supposed to get the integer value from the string and therefore I pass the regex **[^0-9]** for integer numbers. 
 
-```
+```sql
  UPDATE FifaData
 SET W_F = LEFT(W_F, PATINDEX('%[^0-9]%', W_F) - 1),
     SM = LEFT(SM, PATINDEX('%[^0-9]%', SM) - 1),
@@ -124,7 +124,7 @@ SET W_F = LEFT(W_F, PATINDEX('%[^0-9]%', W_F) - 1),
 
  I used a combination of **PATINDEX**, **SUBSTRING** and **REPLACE** to separate the team from the start and end Year. Before the update, I added some extra columns to the dataset for the new columns extracted from the Team_contract.
 
- ```
+ ```sql
 ALTER TABLE FifaData ADD Team varchar(50)
 ALTER TABLE FifaData ADD StartYear int
 ALTER TABLE FifaData ADD EndYear int
@@ -134,7 +134,7 @@ ALTER TABLE FifaData ADD TranserFee varchar(20)
  ```
 
  
-```
+```sql
 UPDATE FifaData
 SET Team = REPLACE(REPLACE(REPLACE(Team_Contract, SUBSTRING(Team_Contract, PATINDEX('%[~]%', Team_Contract) - 5, 5), ''), SUBSTRING(Team_Contract, PATINDEX('%[~]%', Team_Contract) + 1, 5), ''), '~', ''),
     StartYear = SUBSTRING(Team_Contract, PATINDEX('%[~]%', Team_Contract) - 5, 5),
@@ -154,7 +154,7 @@ AND Team_Contract NOT LIKE '%Loan%'
 
 However, stackoverflow came in handy and I was able to figure out that  **CHAR(10)** is a **Line Feed** and replacing that with an empty space worked like charm!
 
- ````
+ ````sql
 UPDATE FifaData
 SET Hits = CAST(REPLACE(REPLACE(Hits, 'K', ''), CHAR(10), '') AS decimal) * 1000
 WHERE ISNUMERIC(Hits) = 0
@@ -167,7 +167,7 @@ WHERE ISNUMERIC(Hits) = 1
 
 Finaly, I ran the script for a few other columns removing the line feed from the columns
 
-```
+```sql
 UPDATE FifaData
 SET IR = CAST(REPLACE( IR, CHAR(10), '') AS INT) 
 ,PAC = CAST(REPLACE( PAC, CHAR(10), '') AS INT)
@@ -181,7 +181,7 @@ SET IR = CAST(REPLACE( IR, CHAR(10), '') AS INT)
 
 Lastly,  I converted the integer columns into INt by running the query below
 
-````
+````sql
 ALTER TABLE FifaData ALTER COLUMN IR INT
 ALTER TABLE FifaData ALTER COLUMN PAC INT
 ALTER TABLE FifaData ALTER COLUMN SHO INT
@@ -198,5 +198,12 @@ ALTER TABLE FifaData ALTER COLUMN SM INT
 
  ### Identifying outliers
 
+In SQL to identify outliers, you can use **ORDER BY** clause to check whether there are values that are much higher or lower than the expected values. Order by will also help identify varying data types in a single column. Having little knowledge on the columns of the dataset, I just performed outlier detection on Age, Weight and Height which are columns that I have a clear understanding of the expected value range.
+For the rest of the columns nothing much I could do. 
+
+This task is tideous with SQL as you would have to go through each column checking for outliers. I believe beyond SQL there are better tools that can easily perform outlier checks across all the dataset columns without having to specify which column. 
 
  ## Conclusion
+
+Data cleaning is a vital process in data analysis as that helps transform data into a format that can be analyzed to draw meaningful insights from data. Its always important to ensure that there no duplicates in your data, columns values that have the different data types unless otherwise stated and there no outliers in your data set.
+Cleaning this data was abit challenging for some columns but despite all that i sailed through using  SQL that provides a wide range of functions helpful in cleaning the data. 
--- script.sql
+++ script.sql
@@ -8,20 +8,17 @@
 -- -- =============================================
 
  
-BEGIN
+-- BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 SET NOCOUNT ON;
 
+-- Insert the data into a temporary table 
 
--- Add rows generated from the data set
+SELECT  *  INTO #FifaData 
+FROM Fifa21
 
-ALTER TABLE FifaData ADD Team varchar(50)
-ALTER TABLE FifaData ADD StartYear int
-ALTER TABLE FifaData ADD EndYear int
-ALTER TABLE FifaData ADD LoanDate varchar(20)
-ALTER TABLE FifaData ADD TranserFee varchar(20) 
-
+GO
 
 
 -- STEP 1: Find and remove duplicates from the dataset using Common Table Expressions(CTE).
@@ -39,7 +36,7 @@ AS (SELECT
   Positions,
   ROW_NUMBER() OVER (PARTITION BY ID, LongName, Name, Nationality, Positions
   ORDER BY id) AS DuplicateCount
-FROM FifaData)
+FROM #FifaData)
 DELETE FROM CTE
 WHERE DuplicateCount > 1
 
@@ -50,7 +47,7 @@ WHERE DuplicateCount > 1
 -- Get the first value on the left which is the height in foot, multiply by 30.48 to convert it to CM
 -- Sum the two values to get the actual height in CM
 
-UPDATE FifaData
+UPDATE #FifaData
 SET Height = (CAST(LEFT(Height, 1) AS int) * 30.48) + (CAST(REPLACE(REPLACE(Height, RIGHT(Height, 1), '')
 , LEFT(Height, 1) + '''', '') AS int) * 2.54)
 
@@ -59,7 +56,7 @@ SET Height = (CAST(LEFT(Height, 1) AS int) * 30.48) + (CAST(REPLACE(REPLACE(Heig
 -- The player weight has a trailing lbs on the right that we should get rid of.
 -- Removing the lbs you are left with the weight in pounds
 -- You can opt to use Kilograms by multiplying the result with 0.454
-UPDATE FifaData
+UPDATE #FifaData
 SET Weight = REPLACE(Weight, 'lbs', '')
 
 
@@ -69,31 +66,31 @@ SET Weight = REPLACE(Weight, 'lbs', '')
 -- Multiply with 10^n where n is represents thousand or  million
 --  
 
-UPDATE FifaData
+UPDATE #FifaData
 SET Value = (
 CASE
-  WHEN RIGHT(Value, 1) = 'M' THEN CAST(REPLACE(REPLACE(Value, '�', ''), RIGHT(Value, 1), '') AS decimal) * 1000000
-  WHEN RIGHT(Value, 1) = 'K' THEN CAST(REPLACE(REPLACE(Value, '�', ''), RIGHT(Value, 1), '') AS decimal) * 1000
+  WHEN RIGHT(Value, 1) = 'M' THEN CAST(REPLACE(REPLACE(Value, '€', ''), RIGHT(Value, 1), '') AS decimal) * 1000000
+  WHEN RIGHT(Value, 1) = 'K' THEN CAST(REPLACE(REPLACE(Value, '€', ''), RIGHT(Value, 1), '') AS decimal) * 1000
   WHEN RIGHT(Value, 1) = '0' THEN 0
-  ELSE (REPLACE(Value, '�', '')
+  ELSE REPLACE(Value, '€', '')
 END)
 
 
-UPDATE FifaData
+UPDATE #FifaData
 SET wage = (CASE
-  WHEN RIGHT(wage, 1) = 'M' THEN CAST(REPLACE(REPLACE(wage, '�', ''), RIGHT(wage, 1), '') AS decimal) * 1000000
-  WHEN RIGHT(wage, 1) = 'K' THEN CAST(REPLACE(REPLACE(wage, '�', ''), RIGHT(wage, 1), '') AS decimal) * 1000
+  WHEN RIGHT(wage, 1) = 'M' THEN CAST(REPLACE(REPLACE(wage, '€', ''), RIGHT(wage, 1), '') AS decimal) * 1000000
+  WHEN RIGHT(wage, 1) = 'K' THEN CAST(REPLACE(REPLACE(wage, '€', ''), RIGHT(wage, 1), '') AS decimal) * 1000
   WHEN RIGHT(wage, 1) = '0' THEN 0
-  ELSE (REPLACE(wage, '�', '')
+  ELSE REPLACE(wage, '€', '')
 END)
 
 
-UPDATE FifaData
+UPDATE #FifaData
 SET Release_Clause = (CASE
-  WHEN RIGHT(Release_Clause, 1) = 'M' THEN CAST(REPLACE(REPLACE(Release_Clause, '�', ''), RIGHT(Release_Clause, 1), '') AS decimal) * 1000000
-  WHEN RIGHT(Release_Clause, 1) = 'K' THEN CAST(REPLACE(REPLACE(Release_Clause, '�', ''), RIGHT(Release_Clause, 1), '') AS decimal) * 1000
+  WHEN RIGHT(Release_Clause, 1) = 'M' THEN CAST(REPLACE(REPLACE(Release_Clause, '€', ''), RIGHT(Release_Clause, 1), '') AS decimal) * 1000000
+  WHEN RIGHT(Release_Clause, 1) = 'K' THEN CAST(REPLACE(REPLACE(Release_Clause, '€', ''), RIGHT(Release_Clause, 1), '') AS decimal) * 1000
   WHEN RIGHT(Release_Clause, 1) = '0' THEN 0
-  ELSE (REPLACE(Release_Clause, '�', '')
+  ELSE REPLACE(Release_Clause, '€', '')
 END)
 
 
@@ -102,7 +99,7 @@ END)
 -- Extract the integer value from these columns using patindex and the LEFT function that gives you the index 
 --and values on the left for a given number of characters
 
-UPDATE FifaData
+UPDATE #FifaData
 SET W_F = LEFT(W_F, PATINDEX('%[^0-9]%', W_F) - 1),
     SM = LEFT(SM, PATINDEX('%[^0-9]%', SM) - 1),
     IR = LEFT(IR, PATINDEX('%[^0-9]%', IR) - 1)
@@ -112,41 +109,50 @@ SET W_F = LEFT(W_F, PATINDEX('%[^0-9]%', W_F) - 1),
  
  -- Split the years from the club and introduce new columns for each as start and end
    
+-- Add rows generated from the data set
+
+ALTER TABLE #FifaData ADD Team varchar(50)
+ALTER TABLE #FifaData ADD StartYear int
+ALTER TABLE #FifaData ADD EndYear int
+ALTER TABLE #FifaData ADD LoanDate varchar(20)
+ALTER TABLE #FifaData ADD TranserFee varchar(20) 
+
+GO
 
 -- Cleaning data in this column requires a combination of SQL functions
 -- First we begin by team contrcats that are not Free and have no loan date.
 -- These rows of data have similar patterns in the data 
 
-UPDATE FifaData
+UPDATE #FifaData
 SET Team = REPLACE(REPLACE(REPLACE(Team_Contract, SUBSTRING(Team_Contract, PATINDEX('%[~]%', Team_Contract) - 5, 5), ''), SUBSTRING(Team_Contract, PATINDEX('%[~]%', Team_Contract) + 1, 5), ''), '~', ''),
     StartYear = SUBSTRING(Team_Contract, PATINDEX('%[~]%', Team_Contract) - 5, 5),
     EndYear = SUBSTRING(Team_Contract, PATINDEX('%[~]%', Team_Contract) + 1, 5)
-FROM FifaData
+FROM #FifaData
 WHERE Team_Contract NOT LIKE '%Free%'
 AND Team_Contract NOT LIKE '%Loan%'
 
 -- Next are the rows that contain the  'Free' word
 
-UPDATE FifaData
+UPDATE #FifaData
 SET Team = REPLACE(Team_Contract, 'Free', ''),
     TranserFee = 'Free'
-FROM FifaData
+FROM #FifaData
 WHERE Team_Contract LIKE '%Free%'
 
 -- Finaly players with Loans
 
-UPDATE FifaData
+UPDATE #FifaData
 SET Team = REPLACE(REPLACE(Team_Contract, 'On Loan', ''),
     RIGHT(CAST(REPLACE(Team_Contract, 'On Loan', '') AS varchar(50)), 15),
     ''),
     LoanDate = RIGHT(CAST(REPLACE(Team_Contract, 'On Loan', '') AS varchar(50)), 15)
-FROM FifaData
+FROM #FifaData
 WHERE Team_Contract LIKE '%Loan%'
 
 
 -- STEP 7 Removing unnecessary columns and validating data types
 
-ALTER TABLE FifaData DROP COLUMN Team_Contract
+ALTER TABLE #FifaData DROP COLUMN Team_Contract
 
 -- Validating and cleaning columns
 
@@ -164,24 +170,24 @@ ALTER TABLE FifaData DROP COLUMN Team_Contract
 --  LEN(Hits),
 --  LEN(REPLACE( Hits, CHAR(10), '')),
 --  CAST(REPLACE(REPLACE(Hits, 'K', ''), CHAR(10), '') AS decimal) * 1000
---FROM FifaData
+--FROM #FifaData
 --WHERE ISNUMERIC(Hits) = 0
 
-UPDATE FifaData
+UPDATE #FifaData
 SET Hits = CAST(REPLACE(REPLACE(Hits, 'K', ''), CHAR(10), '') AS decimal) * 1000
 WHERE ISNUMERIC(Hits) = 0
 
 -- clean line feed from all other columns 
-UPDATE FifaData
+UPDATE #FifaData
 SET Hits = CAST(REPLACE( Hits, CHAR(10), '') AS decimal) 
 WHERE ISNUMERIC(Hits) = 1
 
 -- Convert the column to INT
-ALTER TABLE FifaData ALTER COLUMN Hits INT
+ALTER TABLE #FifaData ALTER COLUMN Hits INT
 
 
 -- Update other integer valued columns  
-UPDATE FifaData
+UPDATE #FifaData
 SET IR = CAST(REPLACE( IR, CHAR(10), '') AS INT) 
 ,PAC = CAST(REPLACE( PAC, CHAR(10), '') AS INT)
 ,SHO = CAST(REPLACE( SHO, CHAR(10), '') AS INT)
@@ -191,76 +197,86 @@ SET IR = CAST(REPLACE( IR, CHAR(10), '') AS INT)
 ,PHY = CAST(REPLACE( PHY, CHAR(10), '') AS INT)
  
 
-ALTER TABLE FifaData ALTER COLUMN IR INT
-ALTER TABLE FifaData ALTER COLUMN PAC INT
-ALTER TABLE FifaData ALTER COLUMN SHO INT
-ALTER TABLE FifaData ALTER COLUMN PAS INT
-ALTER TABLE FifaData ALTER COLUMN DRI INT
-ALTER TABLE FifaData ALTER COLUMN DEF INT
-ALTER TABLE FifaData ALTER COLUMN PHY INT
+ALTER TABLE #FifaData ALTER COLUMN IR INT
+ALTER TABLE #FifaData ALTER COLUMN PAC INT
+ALTER TABLE #FifaData ALTER COLUMN SHO INT
+ALTER TABLE #FifaData ALTER COLUMN PAS INT
+ALTER TABLE #FifaData ALTER COLUMN DRI INT
+ALTER TABLE #FifaData ALTER COLUMN DEF INT
+ALTER TABLE #FifaData ALTER COLUMN PHY INT
 
-ALTER TABLE FifaData ALTER COLUMN SM INT
-ALTER TABLE FifaData ALTER COLUMN W_F INT
-ALTER TABLE FifaData ALTER COLUMN Base_Stats INT
-ALTER TABLE FifaData ALTER COLUMN Total_Stats INT
-ALTER TABLE FifaData ALTER COLUMN GK_Reflexes INT
-ALTER TABLE FifaData ALTER COLUMN GK_Kicking INT
-ALTER TABLE FifaData ALTER COLUMN Gk_Handling INT
-
-
-ALTER TABLE FifaData ALTER COLUMN GK_Diving INT
-ALTER TABLE FifaData ALTER COLUMN GoalKeeping INT
-ALTER TABLE FifaData ALTER COLUMN Sliding_Tackle INT
-ALTER TABLE FifaData ALTER COLUMN Standing_Tackle INT
-ALTER TABLE FifaData ALTER COLUMN Marking INT
-ALTER TABLE FifaData ALTER COLUMN Defending INT
-ALTER TABLE FifaData ALTER COLUMN Composure INT
+ALTER TABLE #FifaData ALTER COLUMN SM INT
+ALTER TABLE #FifaData ALTER COLUMN W_F INT
+ALTER TABLE #FifaData ALTER COLUMN Base_Stats INT
+ALTER TABLE #FifaData ALTER COLUMN Total_Stats INT
+ALTER TABLE #FifaData ALTER COLUMN GK_Reflexes INT
+ALTER TABLE #FifaData ALTER COLUMN GK_Kicking INT
+ALTER TABLE #FifaData ALTER COLUMN Gk_Handling INT
 
 
-ALTER TABLE FifaData ALTER COLUMN Penalties INT
-ALTER TABLE FifaData ALTER COLUMN Vision INT
-ALTER TABLE FifaData ALTER COLUMN Positioning INT
-ALTER TABLE FifaData ALTER COLUMN Interceptions INT
-ALTER TABLE FifaData ALTER COLUMN Aggression INT
-ALTER TABLE FifaData ALTER COLUMN Mentality INT
-ALTER TABLE FifaData ALTER COLUMN Long_Shots INT
+ALTER TABLE #FifaData ALTER COLUMN GK_Diving INT
+ALTER TABLE #FifaData ALTER COLUMN GoalKeeping INT
+ALTER TABLE #FifaData ALTER COLUMN Sliding_Tackle INT
+ALTER TABLE #FifaData ALTER COLUMN Standing_Tackle INT
+ALTER TABLE #FifaData ALTER COLUMN Marking INT
+ALTER TABLE #FifaData ALTER COLUMN Defending INT
+ALTER TABLE #FifaData ALTER COLUMN Composure INT
 
-ALTER TABLE FifaData ALTER COLUMN Strength INT
-ALTER TABLE FifaData ALTER COLUMN Stamina INT
-ALTER TABLE FifaData ALTER COLUMN Jumping INT
-ALTER TABLE FifaData ALTER COLUMN Power INT
-ALTER TABLE FifaData ALTER COLUMN Balance INT
-ALTER TABLE FifaData ALTER COLUMN Reactions INT
-ALTER TABLE FifaData ALTER COLUMN Agility INT
 
-ALTER TABLE FifaData ALTER COLUMN Sprint_Speed INT
-ALTER TABLE FifaData ALTER COLUMN Acceleration INT
-ALTER TABLE FifaData ALTER COLUMN Movement INT
-ALTER TABLE FifaData ALTER COLUMN Ball_Control INT
-ALTER TABLE FifaData ALTER COLUMN Long_Passing INT
-ALTER TABLE FifaData ALTER COLUMN FK_Accuracy INT
-ALTER TABLE FifaData ALTER COLUMN Curve INT
+ALTER TABLE #FifaData ALTER COLUMN Penalties INT
+ALTER TABLE #FifaData ALTER COLUMN Vision INT
+ALTER TABLE #FifaData ALTER COLUMN Positioning INT
+ALTER TABLE #FifaData ALTER COLUMN Interceptions INT
+ALTER TABLE #FifaData ALTER COLUMN Aggression INT
+ALTER TABLE #FifaData ALTER COLUMN Mentality INT
+ALTER TABLE #FifaData ALTER COLUMN Long_Shots INT
 
-ALTER TABLE FifaData ALTER COLUMN Dribbling INT
-ALTER TABLE FifaData ALTER COLUMN Skill INT
-ALTER TABLE FifaData ALTER COLUMN Volleys INT
-ALTER TABLE FifaData ALTER COLUMN Short_Passing INT
-ALTER TABLE FifaData ALTER COLUMN Heading_Accuracy INT
-ALTER TABLE FifaData ALTER COLUMN Finishing INT
-ALTER TABLE FifaData ALTER COLUMN Crossing INT
+ALTER TABLE #FifaData ALTER COLUMN Strength INT
+ALTER TABLE #FifaData ALTER COLUMN Stamina INT
+ALTER TABLE #FifaData ALTER COLUMN Jumping INT
+ALTER TABLE #FifaData ALTER COLUMN Power INT
+ALTER TABLE #FifaData ALTER COLUMN Balance INT
+ALTER TABLE #FifaData ALTER COLUMN Reactions INT
+ALTER TABLE #FifaData ALTER COLUMN Agility INT
 
-ALTER TABLE FifaData ALTER COLUMN Attacking INT
-ALTER TABLE FifaData ALTER COLUMN Growth INT
-ALTER TABLE FifaData ALTER COLUMN BOV INT
-ALTER TABLE FifaData ALTER COLUMN Weight INT
-ALTER TABLE FifaData ALTER COLUMN Height DECIMAL(10,2)
-ALTER TABLE FifaData ALTER COLUMN POT INT
-ALTER TABLE FifaData ALTER COLUMN OVA INT
-ALTER TABLE FifaData ALTER COLUMN Age INT
+ALTER TABLE #FifaData ALTER COLUMN Sprint_Speed INT
+ALTER TABLE #FifaData ALTER COLUMN Acceleration INT
+ALTER TABLE #FifaData ALTER COLUMN Movement INT
+ALTER TABLE #FifaData ALTER COLUMN Ball_Control INT
+ALTER TABLE #FifaData ALTER COLUMN Long_Passing INT
+ALTER TABLE #FifaData ALTER COLUMN FK_Accuracy INT
+ALTER TABLE #FifaData ALTER COLUMN Curve INT
+
+ALTER TABLE #FifaData ALTER COLUMN Dribbling INT
+ALTER TABLE #FifaData ALTER COLUMN Skill INT
+ALTER TABLE #FifaData ALTER COLUMN Volleys INT
+ALTER TABLE #FifaData ALTER COLUMN Short_Passing INT
+ALTER TABLE #FifaData ALTER COLUMN Heading_Accuracy INT
+ALTER TABLE #FifaData ALTER COLUMN Finishing INT
+ALTER TABLE #FifaData ALTER COLUMN Crossing INT
+
+ALTER TABLE #FifaData ALTER COLUMN Attacking INT
+ALTER TABLE #FifaData ALTER COLUMN Growth INT
+ALTER TABLE #FifaData ALTER COLUMN BOV INT
+ALTER TABLE #FifaData ALTER COLUMN Weight INT
+ALTER TABLE #FifaData ALTER COLUMN Height DECIMAL(10,2)
+ALTER TABLE #FifaData ALTER COLUMN POT INT
+ALTER TABLE #FifaData ALTER COLUMN OVA INT
+ALTER TABLE #FifaData ALTER COLUMN Age INT
+
+
+-- STEP 8 Detecting outliers
+
+-- You can use ORDER BY to detect if there any outliers in the columns
+-- For instance to check for outliers in weight, the following query can be used.
+
+-- select Name,Weight from #FifaData order by Weight desc
+
+
 
 -- Display the cleaned data
 
-Select * from FifaData  
+Select * from #FifaData  
 
 
-END
+-- END
